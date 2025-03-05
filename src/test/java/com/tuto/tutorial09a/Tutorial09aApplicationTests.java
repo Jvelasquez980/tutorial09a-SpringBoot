85a4ec2deda7ca2a21b5f81d1ae96b73f8787a2b
JeronimoV <jeronimo.velasquez.escobar@gmail.com>
2025-03-05 06:25:41 -0500

    First Commit

---
+++ src/test/java/com/tuto/tutorial09a/Tutorial09aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuto.tutorial09a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial09aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
